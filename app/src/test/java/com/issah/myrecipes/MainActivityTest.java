f73b5293b3dc89fcb241a01893988b05e238f17b
saitemu_issa <saitemu.issa@student.moringaschool.com>
2021-09-05 20:21:29 +0300

    added robo electric test

---
+++ app/src/test/java/com/issah/myrecipes/MainActivityTest.java
@@ -0,0 +1,30 @@
+package com.issah.myrecipes;
+
+import static org.junit.Assert.assertTrue;
+
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setUp() throws Exception {
+        activity = Robolectric.buildActivity(MainActivity.class)
+                .create()
+                .resume()
+                .get();
+    }
+
+    @Test
+    public void validateTextViewContent(){
+        TextView appNameTextView = activity.findViewById(R.id.appNameTextView);
+        assertTrue("My Recipes".equals(appNameTextView.getText().toString()));
+    }
+}
