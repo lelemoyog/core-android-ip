da237b41853e116463382dd629b38bc83443dd77
saitemu_issa <saitemu.issa@student.moringaschool.com>
2021-09-18 11:13:19 +0300

    added code to receive data from firebase

--- app/src/main/java/com/issah/myrecipes/ui/SavedRecipesActivity.java
+++ app/src/main/java/com/issah/myrecipes/ui/SavedRecipesActivity.java
@@ -3,6 +3,7 @@ package com.issah.myrecipes.ui;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -10,30 +11,89 @@ import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.issah.myrecipes.Constants;
 import com.issah.myrecipes.R;
+import com.issah.myrecipes.adapters.FirebaseRecipeViewHolder;
+import com.issah.myrecipes.models.Hit;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SavedRecipesActivity extends AppCompatActivity {
 
+    private DatabaseReference mRecipeReference;
+    private FirebaseRecyclerAdapter<Hit, FirebaseRecipeViewHolder> mFirebaseAdapter;
 
+    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
+    @BindView(R.id.errorTextView) TextView mErrorTextView;
+    @BindView(R.id.progressBar) ProgressBar mProgressBar;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_findrecipes);
+        setContentView(R.layout.activity_myrecipes);
+        ButterKnife.bind(this);
 
+        mRecipeReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_RECIPES);
 
-
+          setUpFirebaseAdapter();
+          hideProgressBar();
+          ShowRecipes();
 
     }
 
+    private void setUpFirebaseAdapter(){
+        FirebaseRecyclerOptions<Hit>  options = new FirebaseRecyclerOptions.Builder<Hit>()
+                .setQuery(mRecipeReference,Hit.class)
+                .build();
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<Hit, FirebaseRecipeViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull FirebaseRecipeViewHolder holder, int position, @NonNull Hit recipe) {
+                holder.bindRecipe(recipe);
+            }
+
+            @NonNull
+            @Override
+            public FirebaseRecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_list_item,parent,false);
+                return new FirebaseRecipeViewHolder(view);
+            }
+        };
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mFirebaseAdapter!= null) {
+            mFirebaseAdapter.stopListening();
+        }
+    }
+    private void  ShowRecipes() {
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
+    private void hideProgressBar() {
+        mProgressBar.setVisibility(View.GONE);
+    }
 
 
 
