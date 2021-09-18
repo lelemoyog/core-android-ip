3735cdd409696c207a90e6ac0bf13d8dafc96921
saitemu_issa <saitemu.issa@student.moringaschool.com>
2021-09-18 11:22:41 +0300

    added firebase recipe view holder

---
+++ app/src/main/java/com/issah/myrecipes/adapters/FirebaseRecipeViewHolder.java
@@ -0,0 +1,79 @@
+package com.issah.myrecipes.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Parcel;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.issah.myrecipes.Constants;
+import com.issah.myrecipes.R;
+import com.issah.myrecipes.models.Hit;
+import com.issah.myrecipes.ui.IngredientsDetailActivity;
+import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class FirebaseRecipeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+    View mView;
+    Context mContext;
+
+    public FirebaseRecipeViewHolder(View itemView) {
+        super(itemView);
+        mView = itemView;
+        mContext = itemView.getContext();
+        itemView.setOnClickListener(this);
+    }
+
+    public void bindRecipe(Hit recipe) {
+
+        ImageView recipeImageView = (ImageView) mView.findViewById(R.id.recipeimage);
+        TextView labelTextView = (TextView) mView.findViewById(R.id.recipeLabel);
+
+
+        Picasso.get().load(recipe.getRecipe().getImage()).into(recipeImageView);
+        labelTextView.setText(recipe.getRecipe().getLabel());
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        final ArrayList<Hit> recipes = new ArrayList<>();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_RECIPES);
+
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    recipes.add(snapshot.getValue(Hit.class));
+                }
+                int itemPosition = getLayoutPosition();
+
+                Intent intent = new Intent(mContext, IngredientsDetailActivity.class);
+                intent.putExtra("position",itemPosition + "");
+                intent.putExtra("recipes", Parcels.wrap(recipes));
+
+                mContext.startActivity(intent) ;
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+}
