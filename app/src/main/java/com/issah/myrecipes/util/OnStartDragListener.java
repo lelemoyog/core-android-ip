0de1423559d2bba3ced70e284e66ded3e7d9e7da
saitemu_issa <saitemu.issa@student.moringaschool.com>
2021-09-25 10:51:44 +0300

    ItemTouchHelper Setup created needed interfaces

---
+++ app/src/main/java/com/issah/myrecipes/util/OnStartDragListener.java
@@ -0,0 +1,8 @@
+package com.issah.myrecipes.util;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public interface OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
+
+}
